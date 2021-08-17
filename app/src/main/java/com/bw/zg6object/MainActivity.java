4af20ab55d92b415af15b3be6ae9b20473f33d71
November-A <1729550957@qq.com>
2021-08-17 21:11:36 +0800

    合并

--- app/src/main/java/com/bw/zg6object/MainActivity.java
+++ app/src/main/java/com/bw/zg6object/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String a="wwww";
-
         //aaaaa
+        Log.i("cjj", "onCreate: ");
     }
 }
