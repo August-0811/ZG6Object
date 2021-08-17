f9a84316fd82cf25683da5cb8388cba4a5cb5c2e
November-A <1729550957@qq.com>
2021-08-17 20:03:44 +0800

    测试

--- app/src/main/java/com/bw/zg6object/MainActivity.java
+++ app/src/main/java/com/bw/zg6object/MainActivity.java
@@ -4,15 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
-
-
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //张新武是傻逼
+        String a="wwww";
     }
 }
