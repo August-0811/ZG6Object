c4c3c293106dcd095b1135ecda1615dcfc4da168
XCH01 <2057817156@QQ.com>
2021-08-18 19:31:46 +0800

    add

--- app/src/main/java/com/bw/zg6object/MainActivity.java
+++ app/src/main/java/com/bw/zg6object/MainActivity.java
@@ -10,10 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
+
         //来自一个萌新的提交
-=======
->>>>>>> dev
+
 
     }
 }
